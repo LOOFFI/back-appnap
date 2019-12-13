de5123ae10c0bb423ec495ef97a5b051eb4a4a84
LOOFFI <aurelien.rosaz@gmail.com>
2019-12-13 20:02:47 +0100

    deploy

---
+++ public/build/precache-manifest.984296258582ed2f749791920b613951.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22acc022c89fb63047d1e97293583276",
+    "url": "/index.html"
+  },
+  {
+    "revision": "acb202dda53b283b4190",
+    "url": "/static/css/2.b79ede7b.chunk.css"
+  },
+  {
+    "revision": "810a8875af420d498ca9",
+    "url": "/static/css/main.7329e3b2.chunk.css"
+  },
+  {
+    "revision": "acb202dda53b283b4190",
+    "url": "/static/js/2.9f57dec7.chunk.js"
+  },
+  {
+    "revision": "30148d5a1da7dbc32e0ef9b4d8a163df",
+    "url": "/static/js/2.9f57dec7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "810a8875af420d498ca9",
+    "url": "/static/js/main.466ca0d1.chunk.js"
+  },
+  {
+    "revision": "380d84514075e53544ec",
+    "url": "/static/js/runtime-main.571e82bc.js"
+  }
+]);
